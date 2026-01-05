d1f0cc7b15adeca2dfa8f9df87e9fba3af892078
Naba Kumar Das <nabad600@gmail.com>
2026-01-05 15:00:05 +0530

    Update test7.php

--- dtc2/test7.php
+++ dtc2/test7.php
@@ -1,1 +1,13 @@
 jhbljhgluigblikhbikjbjk
+jhgjhgjhvjhvkhbkjhbkjhbkjhoijoiuoiyiuture5463654576876987978576565544344
+
+ENV SECRETSVOL $SECRETSVOL
+ENV JAVA_OPTS $JAVA_OPTS
+ENV JAVA_AGENT $JAVA_AGENT
+
+EXPOSE 8041
+ARG JAR_FILE=target/*.jar
+COPY ${JAR_FILE} /work/app.jar
+
+USER nobody
+CMD java ${JAVA_AGENT} ${JAVA_OPTS} -jar /work
